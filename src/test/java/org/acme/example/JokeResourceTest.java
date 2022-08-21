854f75497b7e63a0839ad9bb9d420cb1c2603db3
ammbra <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>
2022-08-21 22:36:54 +0200

    tests were added

---
+++ src/test/java/org/acme/example/JokeResourceTest.java
@@ -0,0 +1,21 @@
+package org.acme.example;
+import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+@QuarkusTest
+public class JokeResourceTest {
+
+    @Test
+    public void tesJokes() {
+        given()
+                .when().get("/jokes")
+                .then()
+                .statusCode(200)
+                .body(notNullValue());
+    }
+
+}
