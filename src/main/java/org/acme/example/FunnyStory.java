c62edab340b3ce22d32921cbec6de5a3ebbc033c
anamihalceanu <mihalceanuana@gmail.com>
2022-08-30 16:52:42 +0200

    removed commented code

--- src/main/java/org/acme/example/FunnyStory.java
+++ src/main/java/org/acme/example/FunnyStory.java
@@ -6,19 +6,3 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public record FunnyStory(String category, String joke, String setup, String delivery) {
 
 }
-//public class FunnyStory {
-//    public String category;
-//    public String joke;
-//    public String setup;
-//    public String delivery;
-//
-//    @Override
-//    public String toString() {
-//        return "FunnyStory{" +
-//                "category='" + category + '\'' +
-//                ", joke='" + joke + '\'' +
-//                ", setup='" + setup + '\'' +
-//                ", delivery='" + delivery + '\'' +
-//                '}';
-//    }
-//}
